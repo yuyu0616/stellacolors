7fe4e6cd8ab6b8bc1ca75a213771c1b07f06c2bf
yuyu0616 <muraseyusuke16@gmail.com>
2023-03-16 15:59:14 +0900

    header削除

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -37,14 +37,14 @@ export const IndexPageTemplate = ({
                       <h3 className="subtitle">{mainpitch.description}</h3>
                     </div>
                   </div> */}
-                  <div className="columns">
+                  {/* <div className="columns">
                     <div className="column is-12">
                       <h3 className="has-text-weight-semibold is-size-2">
                         {heading}
                       </h3>
                       <p>{description}</p>
                     </div>
-                  </div>
+                  </div> */}
                   <Features gridItems={intro.blurbs} />
                   <div className="columns">
                     <div className="column is-12 has-text-centered">
