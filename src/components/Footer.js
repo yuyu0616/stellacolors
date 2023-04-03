b3f65949fd3783324b1062b6a90d8f359c044b88
yuyu0616 <muraseyusuke16@gmail.com>
2023-04-03 17:10:23 +0900

    フッターロゴ変更

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,9 @@
 import * as React from "react";
 import { Link } from "gatsby";
 
-import logo from "../img/logo.svg";
-import facebook from "../img/social/facebook.svg";
+import logo from "../img/biglogo.png";
 import instagram from "../img/social/instagram.svg";
 import twitter from "../img/social/twitter.svg";
-import vimeo from "../img/social/vimeo.svg";
 import youtube from "../img/social/youtube.png";
 
 const Footer = () => {
@@ -15,7 +13,7 @@ const Footer = () => {
         <div className="content has-text-centered">
           <img
             src={logo}
-            alt="Kaldi"
+            alt="StellaColors"
             style={{ width: "14em", height: "10em" }}
           />
         </div>
@@ -75,13 +73,6 @@ const Footer = () => {
                 </section>
               </div>
               <div className="column is-4 social">
-                <a title="facebook" href="https://facebook.com">
-                  <img
-                    src={facebook}
-                    alt="Facebook"
-                    style={{ width: "1em", height: "1em" }}
-                  />
-                </a>
                 <a title="twitter" href="https://twitter.com">
                   <img
                     className="fas fa-lg"
